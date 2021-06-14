9dc1a810479c7fc850fb2fea2379cc4949fb050f
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-14 16:36:04 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/recipeWeb/RecipeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipeWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
