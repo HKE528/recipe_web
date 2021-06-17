06737e38271bf2a0b7dc13d8f94f944e9ffea22e
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-17 11:15:02 +0900

    레시피 리포지토리 생성

---
+++ src/main/java/com/example/recipeWeb/repository/RecipeRepository.java
@@ -0,0 +1,22 @@
+package com.example.recipeWeb.repository;
+
+import com.example.recipeWeb.domain.Recipe;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+
+public class RecipeRepository {
+    private final EntityManager em;
+
+    public void save(Recipe recipe) {
+        em.persist(recipe);
+    }
+
+    public Recipe findOne(int id) {
+        return em.find(Recipe.class, id);
+    }
+}
