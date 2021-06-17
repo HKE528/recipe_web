1743cc783926d4ba8bc97df6f9b954eb0c95e7d3
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-17 11:15:16 +0900

    레시피 서비스 생성

---
+++ src/main/java/com/example/recipeWeb/service/RecipeService.java
@@ -0,0 +1,36 @@
+package com.example.recipeWeb.service;
+
+import com.example.recipeWeb.DTO.RecipeDTO;
+import com.example.recipeWeb.domain.Recipe;
+import com.example.recipeWeb.repository.RecipeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RecipeService {
+    private final RecipeRepository recipeRepository;
+
+    public int saveRecipe(RecipeDTO recipeDTO) {
+        Recipe recipe = new Recipe(recipeDTO);
+
+        recipeRepository.save(recipe);
+
+        return recipe.getId();
+    }
+
+    public RecipeDTO loadRecipe(int id) {
+        Recipe recipe = recipeRepository.findOne(id);
+
+        RecipeDTO recipeDTO = new RecipeDTO(
+                recipe.getName(),
+                recipe.getCategory(),
+                recipe.getIngredient(),
+                recipe.getDescription()
+        );
+
+        return recipeDTO;
+    }
+}
