e4f15ef9aa3664d332ab93a0c2b3119d04b3a592
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-17 11:41:16 +0900

    MyRecipes 리포지토리

---
+++ src/main/java/com/example/recipeWeb/repository/MyRecipeRepository.java
@@ -0,0 +1,30 @@
+package com.example.recipeWeb.repository;
+
+import com.example.recipeWeb.domain.Member;
+import com.example.recipeWeb.domain.MyRecipes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MyRecipeRepository {
+    private final EntityManager em;
+
+    public void save(MyRecipes myRecipes) {
+        em.persist(myRecipes);
+    }
+
+    public MyRecipes findOne(int id) {
+        return em.find(MyRecipes.class, id);
+    }
+
+    public List<MyRecipes> findAll(Member member) {
+        return em.createQuery("select mr from MyRecipes mr where member = :member", MyRecipes.class)
+                .setParameter("member", member)
+                .getResultList();
+    }
+
+}
