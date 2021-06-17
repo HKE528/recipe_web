25a109e508f7e8d16e690d04c2fb606ee37cba08
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-17 11:41:44 +0900

    MyRecipeService 생성

---
+++ src/main/java/com/example/recipeWeb/service/MyRecipeService.java
@@ -0,0 +1,35 @@
+package com.example.recipeWeb.service;
+
+import com.example.recipeWeb.DTO.MemberDTO;
+import com.example.recipeWeb.DTO.RecipeDTO;
+import com.example.recipeWeb.domain.Member;
+import com.example.recipeWeb.domain.MyRecipes;
+import com.example.recipeWeb.domain.Recipe;
+import com.example.recipeWeb.repository.MemberRepository;
+import com.example.recipeWeb.repository.MyRecipeRepository;
+import com.example.recipeWeb.repository.RecipeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MyRecipeService {
+    private final MyRecipeRepository myRecipeRepository;
+    private final RecipeRepository recipeRepository;
+    private final MemberRepository memberRepository;
+    private final RecipeService recipeService;
+
+    public int createMyRecipe(String memberId, RecipeDTO recipeDTO) {
+        int recipeId = recipeService.saveRecipe(recipeDTO);
+        Recipe recipe = recipeRepository.findOne(recipeId);
+        Member member = memberRepository.findOne(memberId);
+
+        MyRecipes myRecipes = new MyRecipes(member, recipe);
+
+        myRecipeRepository.save(myRecipes);
+
+        return myRecipes.getId();
+    }
+}
