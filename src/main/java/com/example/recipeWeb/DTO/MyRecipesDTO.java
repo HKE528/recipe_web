539060de8329b92dc1fa802efef483ed58276f84
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-18 10:41:14 +0900

    dto 추가

---
+++ src/main/java/com/example/recipeWeb/DTO/MyRecipesDTO.java
@@ -0,0 +1,32 @@
+package com.example.recipeWeb.DTO;
+
+import com.example.recipeWeb.domain.Member;
+import com.example.recipeWeb.domain.Recipe;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class MyRecipesDTO {
+    private int id;
+    private LocalDate addDate;
+    private Member member;
+    private Recipe recipe;
+
+    public MyRecipesDTO() {
+    }
+
+    public MyRecipesDTO(int id, LocalDate addDate) {
+        this.id = id;
+        this.addDate = addDate;
+    }
+
+    public MyRecipesDTO(int id, LocalDate addDate, Member member, Recipe recipe) {
+        this.id = id;
+        this.addDate = addDate;
+        this.member = member;
+        this.recipe = recipe;
+    }
+}
