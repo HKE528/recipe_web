40214da5eaad7a15880b4b1861a4eb8f8106a2e0
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-21 10:41:20 +0900

    LoingDTO valid 옵션 추가

--- src/main/java/com/example/recipeWeb/DTO/LoginDTO.java
+++ src/main/java/com/example/recipeWeb/DTO/LoginDTO.java
@@ -3,9 +3,13 @@ package com.example.recipeWeb.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class LoginDTO {
+
+    @NotNull
     private String id;
     private String pw;
 }
