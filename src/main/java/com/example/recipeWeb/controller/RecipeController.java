494a4cd77767f6de3e286e557c7e208e534d4365
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-06-22 15:44:22 +0900

    레시피 컨트롤러 구현

---
+++ src/main/java/com/example/recipeWeb/controller/RecipeController.java
@@ -0,0 +1,49 @@
+package com.example.recipeWeb.controller;
+
+import com.example.recipeWeb.DTO.MemberDTO;
+import com.example.recipeWeb.DTO.RecipeDTO;
+import com.example.recipeWeb.service.MemberService;
+import com.example.recipeWeb.service.MyRecipeService;
+import com.example.recipeWeb.service.RecipeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class RecipeController {
+    private final RecipeService recipeService;
+    private final MemberService memberService;
+    private final MyRecipeService myRecipeService;
+
+    @GetMapping("recipe/{memberId}/my")
+    public String myRecipe(@Nullable @PathVariable("memberId") String memberId, Model model) {
+        MemberDTO memberDTO = memberService.findOne(memberId);
+
+        model.addAttribute("member", memberDTO);
+
+        return "recipe/myRecipe";
+    }
+
+    @GetMapping("recipe/{memberId}/my/add")
+    public String addRecipe(@PathVariable("memberId") String memberId, Model model) {
+        model.addAttribute("id", memberId);
+
+        return "recipe/addRecipeForm";
+    }
+
+    @PostMapping("recipe/{memberId}/my/add")
+    public String add(@PathVariable("memberId") String memberId, @ModelAttribute("form")RecipeDTO recipeDTO) {
+        int myRecipe = myRecipeService.createMyRecipe(memberId, recipeDTO);
+
+        return "redirect:/recipe/"+ memberId + "/my";
+    }
+}
