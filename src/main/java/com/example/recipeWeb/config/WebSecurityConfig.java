191afae31ac8879816fc6d978e0c5330c89910b4
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-01 14:52:33 +0900

    스프링 시큐리티 설정 추가

--- src/main/java/com/example/recipeWeb/config/WebSecurityConfig.java
+++ src/main/java/com/example/recipeWeb/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/home", "/css/**", "/image/**", "/js/**").permitAll()
+                    .antMatchers("/", "/member/register", "/css/**", "/image/**", "/js/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
