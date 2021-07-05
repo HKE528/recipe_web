1501a7d7b4d601c266c3fcd996ffa8838f32fc1f
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-05 16:46:33 +0900

    no message

--- src/main/java/com/example/recipeWeb/repository/RecipeRepository.java
+++ src/main/java/com/example/recipeWeb/repository/RecipeRepository.java
@@ -3,5 +3,7 @@ package com.example.recipeWeb.repository;
 import com.example.recipeWeb.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 }
