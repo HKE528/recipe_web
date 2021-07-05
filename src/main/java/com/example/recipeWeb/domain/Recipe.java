90a83f201d28561d412bc2d04ce2df6c9f2bc4ed
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-05 16:46:12 +0900

    카테고리 저장 방식 수정

--- src/main/java/com/example/recipeWeb/domain/Recipe.java
+++ src/main/java/com/example/recipeWeb/domain/Recipe.java
@@ -1,5 +1,6 @@
 package com.example.recipeWeb.domain;
 
+import com.example.recipeWeb.domain.dto.RecipeDTO;
 import com.example.recipeWeb.domain.enums.CategoryEnum;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,7 +19,7 @@ public class Recipe {
 
     @Id @GeneratedValue
     @Column(name = "recipe_id")
-    private int id;
+    private Long id;
 
     @Column(name="recipe_name", length = 40, nullable = false)
     private String name;
@@ -39,10 +40,54 @@ public class Recipe {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private Category category;
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "category_id")
+    @Enumerated(EnumType.STRING)
+    private CategoryEnum category;
 
     @OneToOne(fetch = FetchType.LAZY, mappedBy = "recipe", cascade = CascadeType.ALL)
     private RecipeInfo recipeInfo;
+
+    public Recipe(String name, String ingredient, String description, String comment, boolean shareable, CategoryEnum category) {
+        this.name = name;
+        this.ingredient = ingredient;
+        this.description = description;
+        this.comment = comment;
+        this.shareable = shareable;
+        this.category = category;
+    }
+
+    public static Recipe createRecipe(RecipeDTO dto, Member member, RecipeInfo recipeInfo) {
+        Recipe recipe = new Recipe(
+                dto.getName(),
+                dto.getIngredient(),
+                dto.getDescription(),
+                dto.getComment(),
+                dto.isShareable(),
+                dto.getCategory()
+        );
+        
+        recipe.setRecipeInfo(recipeInfo);
+        recipe.setMember(member);
+
+        return recipe;
+    }
+
+    public void changeData(RecipeDTO dto) {
+        this.name = dto.getName();
+        this.ingredient = dto.getDescription();
+        this.description = dto.getDescription();
+        this.comment = dto.getComment();
+        this.category = dto.getCategory();
+    }
+
+    private void setMember(Member member) {
+        this.member = member;
+        member.getRecipes().add(this);
+    }
+
+    private void setRecipeInfo(RecipeInfo recipeInfo) {
+        this.recipeInfo = recipeInfo;
+        recipeInfo.setRecipe(this);
+    }
 }
