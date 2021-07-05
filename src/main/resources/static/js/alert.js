b18e35d7f76ad0121c0c146b3ad614f5bb8d856d
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-07-05 14:59:44 +0900

    삭제 확인 스크립트 추가

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -1,7 +1,15 @@
-function confirmDelete(memberId, recipeId){
+function confirmDeleteRecipe(memberId, recipeId){
     var resp = confirm("정말로 삭제할까요?");
 
     if(resp) {
         location.href="/recipe/" + memberId +"/my/delete/"+ recipeId;
+    }
+}
+
+function confirmDeleteMember(){
+    var resp = confirm("정말로 탈퇴 하시겠습니까?");
+
+    if(resp) {
+        location.href="/recipe/drop;
     }
 }
